99f98b1da3501bf8f5974830cef63ef2d721830f
protatodev <thaddonaghue@yahoo.com>
2018-09-10 12:49:43 -0700

    continue to build out front-end

--- src/components/ToyApp.js
+++ src/components/ToyApp.js
@@ -7,16 +7,34 @@ class ToyApp extends React.Component {
   }
   handleViewSequence = (e) => {
     const digits = e.target.elements.option.value.trim();
-    this.setState(() => ({ sequence: seq}))
-  }
+    const promise = this.getSequence(digits);
 
-  getSequence() {
+    promise.then(seq => {
+      this.setState(() => ({ sequence: seq.sequence}));
+    }).catch(err => {
+      console.log(err);
+    });
     
-  }  
-
-  componentDidMount() {
-    const promise = getSequence();
   }
+
+  getSequence(digits) {
+    return new Promise((resolve,reject) => {
+      const request = new XMLHttpRequest();
+
+      request.open('GET', `http://localhost:8080/fibonacci/${digits}`, true);
+
+      request.onload = function() {
+        if(request.status === 200) {
+          const sequence = JSON.parse(request.response);
+          resolve(sequence);
+        } else {
+          reject(request.statusText);
+        }
+      }
+
+      request.send();
+    });
+  }  
 
   render() {
     return(
@@ -26,7 +44,7 @@ class ToyApp extends React.Component {
           <input type="number" placeholder="e.g, 10"/>
           <button type="submit">View Fibonacci Sequence</button>
         </form>
-       
+        {this.state.sequence.length > 0 && this.state.sequence.map(number => <p>{number}</p>)}
       </div>
     );
   }
